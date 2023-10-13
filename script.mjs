a4517f8b90f421d8c96e9090ee39c3dbecd46a73
iabdulb <abdulbasitm53@gmail.com>
2023-10-13 17:29:23 +0500

    learning hellow world javascript

---
+++ script.mjs
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+let myNumber = 5;
+let myName = "Abdul Basit"
+
+console.log(myNumber);
+console.log(myName);
+
+myNumber = 100;
+myName = "Abdul Basit";
+
+console.log(myNumber);
+console.log(myName);
