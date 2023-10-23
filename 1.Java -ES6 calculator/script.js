c5569d81dde38351016d1e4847981579ac38ba7a
iabdulb <abdulbasitm53@gmail.com>
2023-10-23 21:06:38 +0500

    an ecommerce website for the Motorcycle dealer

--- 1.Java -ES6 calculator/script.js
+++ 1.Java -ES6 calculator/script.js
@@ -1,13 +1,14 @@
-function convertToCelsius() {
-    var fahrenheit = parseFloat(document.getElementById('fahrenheitInput').value);
-    var celsius = (fahrenheit - 32) * 5 / 9;
-    document.getElementById('celsiusInput').value = celsius.toFixed(2);
-    document.getElementById('result').innerText = celsius.toFixed(2) + '°C';
-}
-
-function convertToFahrenheit() {
-    var celsius = parseFloat(document.getElementById('celsiusInput').value);
-    var fahrenheit = celsius * 9 / 5 + 32;
-    document.getElementById('fahrenheitInput').value = fahrenheit.toFixed(2);
-    document.getElementById('result').innerText = fahrenheit.toFixed(2) + '°F';
-}
+const products = document.querySelectorAll('.product');
+        let currentProductIndex = 0;
+        
+        function showNextProduct() {
+            products[currentProductIndex].style.display = 'none';
+            currentProductIndex = (currentProductIndex + 1) % products.length;
+            products[currentProductIndex].style.display = 'block';
+        }
+        
+        // Initially display the first product
+        products[currentProductIndex].style.display = 'block';
+        
+        // Automatically transition to the next product every 3 seconds
+        setInterval(showNextProduct, 3000);
